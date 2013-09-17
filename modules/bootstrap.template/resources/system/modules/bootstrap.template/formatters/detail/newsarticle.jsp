731bd21d693153dd484cadab78f9be68cef7c21c
Ruediger Kurz <r.kurz@alkacon.com>
2013-09-17 10:03:33 +0200

    Removed not any longer used modules.

--- modules/bootstrap.template/resources/system/modules/bootstrap.template/formatters/detail/newsarticle.jsp
+++
@@ -1,7 +0,0 @@
-<%@page buffer="none" session="false" taglibs="cms"%><%--
---%><cms:formatter var="content" val="value" rdfa="rdfa">
-      <div class="adediv">
-        <h1 ${content.rdfa.Title}>${value.Title}</h1>
-      </div><%--
---%></cms:formatter>
-    
--- src-modules/com/alkacon/bootstrap/clone/CmsCloneModule.java
+++ src-modules/com/alkacon/bootstrap/clone/CmsCloneModule.java
@@ -29,7 +29,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package bootstrap.template;
+package com.alkacon.bootstrap.clone;
 
 import org.opencms.configuration.CmsConfigurationCopyResource;
 import org.opencms.configuration.Messages;
