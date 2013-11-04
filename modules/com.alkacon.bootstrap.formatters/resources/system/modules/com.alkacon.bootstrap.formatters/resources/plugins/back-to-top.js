47f7cd67fb247080a5282df4ece292ba4a429e66
Ruediger Kurz <r.kurz@alkacon.com>
2013-11-04 14:32:02 +0100

    Synchronized demo modules with server state.

--- modules/com.alkacon.bootstrap.formatters/resources/system/modules/com.alkacon.bootstrap.formatters/resources/plugins/back-to-top.js
+++ modules/com.alkacon.bootstrap.formatters/resources/system/modules/com.alkacon.bootstrap.formatters/resources/plugins/back-to-top.js
@@ -9,7 +9,7 @@ var scrolltotop={
 	//startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control
 	//scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).
 	setting: {startline:100, scrollto: 0, scrollduration:1000, fadeduration:[500, 100]},
-	controlHTML: '<img src="<cms:link>%(link.weak:/system/modules/com.alkacon.bootstrap.formatters/resources/img/up.png)</cms:link>" style="width:51px; height:42px" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
+	controlHTML: '<img src="<cms:link>%(link.weak:/system/modules/com.alkacon.bootstrap.formatters/resources/img/up.png:11fa3b90-3a3b-11e3-a584-000c2943a707)</cms:link>" style="width:51px; height:42px" />', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
 	controlattrs: {offsetx:5, offsety:5}, //offset of control relative to right/ bottom of window corner
 	anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links
 
