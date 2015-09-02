a7de98bf31f329d6b9e85a3e6f8d6c4455acee00
aZahner <a.zahner@alkacon.com>
2015-09-02 14:43:42 +0200

    Initial version of Unify basics and formatters modules.

---
+++ modules/com.alkacon.unify.basics/Gruntfile.js
@@ -0,0 +1,144 @@
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    'compile-handlebars': {
+       
+       template: {
+         template: 'handlebars/*.hb',
+         templateData: 'template/*.json',
+         partials: ['handlebars/parts/*.hb', 'handlebars/components/*.hb'],
+         output: 'output/*.html',
+         files: [{
+             expand: true,
+             flatten: true,
+             src: 'handlebars/*.hb',
+             dest: 'output/',
+             ext: '.html'
+         }],
+       },
+     },
+
+    sass: {
+	    dist: {
+	      options: {
+	    	  sourcemap: 'auto',
+	    	  lineNumbers: true, 
+	    	  style: 'nested',
+	      },
+	      files: [{
+	    	  expand: true,
+	    	  cwd: 'components/scss',
+	    	  src: ['style-blue.scss'],
+	    	  dest: 'output/resources/css',
+	    	  ext: '.css'
+	      }]
+	    }
+	  },
+
+	cssmin: {
+	  options: {
+		shorthandCompacting: true,
+		roundingPrecision: -1
+	  },
+	  target: {
+		files: [
+		{
+		  'output/resources/css/style-blue.min.css': [
+			'output/resources/css/style-blue.css', 
+			'!output/resources/css/*.min.css'
+		  ]
+		},
+		{
+			'output/resources/css/styles-main.min.css': [
+				'components/css/fonts.css',
+				'components/plugins/bootstrap/css/bootstrap.min.css', 
+				'components/css/style.css',
+				'components/css/headers/header-default.css',
+				'components/css/footers/footer-v1.css',
+				'components/plugins/animate.css',
+	    		'components/plugins/line-icons/line-icons.css',
+	    		'components/plugins/font-awesome/css/font-awesome.min.css',
+	   			'components/plugins/parallax-slider/css/parallax-slider.css',
+	    		'components/plugins/owl-carousel/owl-carousel/owl.carousel.css',
+				'!output/resources/css/*.min.css'
+			  ]
+		}
+		
+		]
+	  }
+	},
+
+	uglify: {
+		my_target: {
+		  files: {
+		    'output/resources/js/scripts-all.min.js': [
+		
+				'components/plugins/jquery/jquery.min.js',
+				'components/plugins/jquery/jquery-migrate.min.js',
+				'components/plugins/bootstrap/js/bootstrap.min.js',
+				'components/plugins/bootstrap-paginator/bootstrap-paginator.min.js',
+				'components/plugins/back-to-top.js',
+				'components/plugins/smoothScroll.js',
+				'components/plugins/parallax-slider/js/modernizr.js',
+				'components/plugins/parallax-slider/js/jquery.cslider.js',
+				'components/plugins/owl-carousel/owl-carousel/owl.carousel.js',
+				'components/js/app.js',
+				'components/js/plugins/owl-carousel.js',
+				'components/js/plugins/parallax-slider.js'
+			]
+		  }
+		}
+	  },
+	  
+    copy: {
+	  main: {
+		  files: [{
+			  expand: true, 
+			  flatten: true, 
+			  src: ['output/resources/css/*.min.css'], 
+			  dest: '/home/user/opencms-mount/docker-2/demo/system/modules/com.alkacon.unify.basics/resources/css/', 
+			  filter: 'isFile'
+		},{
+			  expand: true, 
+			  flatten: true, 
+			  src: ['output/resources/js/*.min.js'], 
+			  dest: '/home/user/opencms-mount/docker-2/demo/system/modules/com.alkacon.unify.basics/resources/js/', 
+			  filter: 'isFile'
+		}],
+	  },
+	},	  
+
+    watch: {
+		handlebars: {
+			files: '**/*.hb',
+	   	   	tasks: ['compile-handlebars']	
+		},
+		scss: {
+			files: '**/*.scss',
+			tasks: ['sass', 'cssmin', 'copy']
+		},
+		uglify: {
+			files: 'components/**/*.js',
+			tasks: ['uglify', 'copy']
+		}
+    },
+
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-compile-handlebars');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  
+  // Default task.
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
+
+};
---
+++ modules/com.alkacon.unify.formatters/resources/system/modules/com.alkacon.unify.formatters/templates/unify-page.jsp
@@ -0,0 +1,65 @@
+<%@page buffer="none" session="false" trimDirectiveWhitespaces="true" %><%--
+--%><%@ taglib prefix="cms" uri="http://www.opencms.org/taglib/cms" %><%--
+--%><%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %><%--
+--%><%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %><%--
+--%><%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %><%--
+--%><fmt:setLocale value="${cms.locale}" /><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>OpenCms | ${cms.title}</title>
+	
+	<meta charset="${cms.requestContext.encoding}">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta name="description" content="<cms:property name="Description" file="search" default="" />">
+	<meta name="keywords" content="<cms:property name="Keywords" file="search" default="" />">
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="7 days">
+
+	<link rel="apple-touch-icon" sizes="120x120" href="<cms:link>%(link.weak:/system/modules/com.alkacon.unify.basics/resources/img/favicon_120.png:0c514573-5159-11e5-abeb-0242ac11002b)</cms:link>"/>
+	<link rel="shortcut icon" href="<cms:link>%(link.weak:/system/modules/com.alkacon.unify.basics/resources/img/favicon_16.png:0c536856-5159-11e5-abeb-0242ac11002b)</cms:link>" type="image/png"/>
+
+	<cms:enable-ade/>
+	
+	<cms:headincludes type="css" closetags="false" defaults="%(link.weak:/system/modules/com.alkacon.unify.basics/resources/css/styles-main.min.css:0fa43d45-5155-11e5-abeb-0242ac11002b)" />
+
+	<c:set var="colortheme"><cms:property name="unify.page.color" file="search" default="blue" /></c:set>
+	<link rel="stylesheet" href="<cms:link>/system/modules/com.alkacon.unify.basics/resources/css/style-${colortheme}.min.css</cms:link>">
+
+	
+</head><body>
+<div class="wrapper">
+<c:if test="${cms.isEditMode}">
+	<!--=== Placeholder for OpenCms toolbar in edit mode ===-->
+	<div style="background: lightgray; height: 50px;">&nbsp;</div>
+</c:if>
+
+<cms:container name="page-complete" type="page" width="1200" maxElements="15" editableby="ROLE.DEVELOPER">
+    <cms:bundle basename="com.alkacon.unify.formatters.messages">
+        <div class="service-block rounded-3x service-block-red">
+            <h2 class="heading-md">
+				<fmt:message key="unify.page.headline.emptycontainer"/>
+				<div class="oc-label-developer">DEVELOPER</div>
+			</h2>
+            <p><fmt:message key="unify.page.text.emptycontainer"/></p>           
+        </div>
+    </cms:bundle>
+</cms:container>
+
+</div><!--/wrapper-->
+
+<%-- JavaScript files placed at the end of the document so the pages load faster --%>
+<cms:headincludes type="javascript" defaults="%(link.weak:/system/modules/com.alkacon.unify.basics/resources/js/scripts-all.min.js:0fc90357-5155-11e5-abeb-0242ac11002b)" />
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+      	App.init();
+    });
+</script>
+<!--[if lt IE 9]>
+    <script src="<cms:link>%(link.weak:/system/modules/com.alkacon.unify.basics/resources/compatibility/respond.js:164f5662-515b-11e5-abeb-0242ac11002b)</cms:link>"></script>
+    <script src="<cms:link>%(link.weak:/system/modules/com.alkacon.unify.basics/resources/compatibility/html5shiv.js:163824de-515b-11e5-abeb-0242ac11002b)</cms:link>"></script>
+    <script src="<cms:link>%(link.weak:/system/modules/com.alkacon.unify.basics/resources/compatibility/placeholder-IE-fixes.js:16423700-515b-11e5-abeb-0242ac11002b)</cms:link>"></script>
+<![endif]-->
+</body>
+</html>
